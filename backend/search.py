bce9e34d07ca4eb957d2ecf191263181ce5a19f3
Bary Levi <barylevi@gmail.com>
2023-03-07 13:33:52 +0200

    question answering

--- backend/search.py
+++ backend/search.py
@@ -1,7 +1,9 @@
 import torch
 import logging
+import nltk
 
 from typing import List
+from dataclasses import dataclass
 from sentence_transformers import SentenceTransformer, CrossEncoder
 
 import integrations_api
@@ -14,15 +16,36 @@ from models import bi_encoder, cross_encoder_small, cross_encoder_large
 BI_ENCODER_CANDIDATES = 100 if torch.cuda.is_available() else 50
 SMALL_CROSS_ENCODER_CANDIDATES = 20 if torch.cuda.is_available() else 10
 
+nltk.download('punkt')
 
-def _split_into_paragraphs(text, minimum_length=256):
+
+@dataclass
+class ResultPresentation:
+    content: str
+    bold: bool
+
+
+@dataclass
+class Candidate:
+    content: str
+    score: float = 0.0
+    representation: List[ResultPresentation] = None
+
+    def to_api(self) -> dict:
+        return {
+            'score': self.score,
+            'content': self.representation
+        }
+
+
+def _split_into_paragraphs(text, minimum_length=768):
     """
     split into paragraphs and batch small paragraphs together into the same paragraph
     """
     paragraphs = []
     current_paragraph = ''
     for paragraph in text.split('\n\n'):
-        current_paragraph += paragraph
+        current_paragraph += ' ' + paragraph
 
         if len(current_paragraph) > minimum_length:
             paragraphs.append(current_paragraph)
@@ -73,18 +96,34 @@ def index_documents(documents: List[integrations_api.BasicDocument]) -> List[Par
 def _cross_encode(
         cross_encoder: CrossEncoder,
         query: str,
-        paragraphs: List[Paragraph],
-        top_k: int) -> List[Paragraph]:
-    scores = cross_encoder.predict([(query, paragraph.content) for paragraph in paragraphs])
-    candidates = [{
-        'paragraph': paragraph,
-        'score': score
-    } for paragraph, score in zip(paragraphs, scores)]
-    candidates.sort(key=lambda c: c['score'], reverse=True)
-    return [candidate['paragraph'] for candidate in candidates[:top_k]]
+        candidates: List[Candidate],
+        top_k: int) -> List[Candidate]:
+    scores = cross_encoder.predict([(query, candidate.content) for candidate in candidates])
+    for candidate, score in zip(candidates, scores):
+        candidate.score = score.item()
+    candidates.sort(key=lambda c: c.score, reverse=True)
+    return candidates[:top_k]
 
 
-def search_documents(query: str, top_k: int) -> List[Paragraph]:
+def _find_answers_in_candidates(candidates: List[Candidate], query: str) -> List[Candidate]:
+    for candidate in candidates:
+        sentences = nltk.sent_tokenize(candidate.content)
+        scores = cross_encoder_small.predict([(query, sent) for sent in sentences], convert_to_tensor=True)
+        best_sentence = torch.argmax(scores).item()
+
+        candidate.representation = []
+        if best_sentence > 0:
+            prefix = ' '.join(sentences[:best_sentence])
+            candidate.representation.append(ResultPresentation(prefix, False))
+        candidate.representation.append(ResultPresentation(sentences[best_sentence], True))
+        if best_sentence < len(sentences) - 1:
+            suffix = ' '.join(sentences[best_sentence + 1:])
+            candidate.representation.append(ResultPresentation(suffix, False))
+
+    return candidates
+
+
+def search_documents(query: str, top_k: int) -> List[dict]:
     # Encode the query
     query_embedding = bi_encoder.encode(query, convert_to_tensor=True)
 
@@ -98,9 +137,10 @@ def search_documents(query: str, top_k: int) -> List[Paragraph]:
         paragraphs = session.query(Paragraph).filter(Paragraph.id.in_(results)).all()
         if len(paragraphs) == 0:
             return []
-
+        candidates = [Candidate(content=paragraph.content, score=0.0) for paragraph in paragraphs]
         # calculate small cross-encoder scores to leave just a few candidates
-        candidates = _cross_encode(cross_encoder_small, query, paragraphs, SMALL_CROSS_ENCODER_CANDIDATES)
+        candidates = _cross_encode(cross_encoder_small, query, candidates, SMALL_CROSS_ENCODER_CANDIDATES)
         # calculate large cross-encoder scores to leave just top_k candidates
         candidates = _cross_encode(cross_encoder_large, query, candidates, top_k)
-        return candidates
+        candidates = _find_answers_in_candidates(candidates, query)
+        return [candidate.to_api() for candidate in candidates]
