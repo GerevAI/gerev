f1f77faa62b35e53eaf6bc23653842060c1ff12b
Bary Levi <barylevi@gmail.com>
2023-03-07 18:30:08 +0200

    add bm25 search alongside semantic search

---
+++ backend/indexing/bm25_index.py
@@ -0,0 +1,61 @@
+import logging
+import os
+import pickle
+import nltk
+import numpy as np
+from rank_bm25 import BM25Okapi
+from typing import List
+
+from db_engine import Session
+from schemas import Paragraph
+
+INDEX_PATH = '/tmp/storage/bm25_index.bin'
+
+
+class Bm25Index:
+    instance = None
+
+    @staticmethod
+    def create():
+        if Bm25Index.instance is not None:
+            raise RuntimeError("Index is already initialized")
+
+        if os.path.exists(INDEX_PATH):
+            with open(INDEX_PATH, 'rb') as f:
+                Bm25Index.instance = pickle.load(f)
+        else:
+            Bm25Index.instance = Bm25Index()
+
+    @staticmethod
+    def get() -> 'Bm25Index':
+        if Bm25Index.instance is None:
+            raise RuntimeError("Index is not initialized")
+        return Bm25Index.instance
+
+    def __init__(self) -> None:
+        self.index = None
+        self.id_map = []
+
+    def update(self):
+        with Session() as session:
+            all_paragraphs = session.query(Paragraph).all()
+            corpus = [nltk.word_tokenize(paragraph.content) for paragraph in all_paragraphs]
+            id_map = [paragraph.id for paragraph in all_paragraphs]
+            self.index = BM25Okapi(corpus)
+            self.id_map = id_map
+        with open(INDEX_PATH, 'wb') as f:
+            pickle.dump(self, f)
+
+    def search(self, query: str, top_k: int) -> List[int]:
+        tokenized_query = nltk.word_tokenize(query)
+        bm25_scores = self.index.get_scores(tokenized_query)
+        top_n = np.argpartition(bm25_scores, -top_k)[-top_k:]
+        bm25_hits = [{'id': self.id_map[idx], 'score': bm25_scores[idx]} for idx in top_n]
+        bm25_hits = sorted(bm25_hits, key=lambda x: x['score'], reverse=True)
+        return [hit['id'] for hit in bm25_hits]
+
+    def clear(self):
+        self.index = None
+        self.id_map = []
+        with open(INDEX_PATH, 'wb') as f:
+            pickle.dump(self, f)
--- backend/search.py
+++ backend/search.py
@@ -8,9 +8,11 @@ from sentence_transformers import CrossEncoder
 from schemas import Paragraph
 from db_engine import Session
 from indexing.faiss_index import FaissIndex
+from indexing.bm25_index import Bm25Index
 from models import bi_encoder, cross_encoder_small, cross_encoder_large, qa_model
 
-BI_ENCODER_CANDIDATES = 100 if torch.cuda.is_available() else 50
+BM_25_CANDIDATES = 50 if torch.cuda.is_available() else 20
+BI_ENCODER_CANDIDATES = 50 if torch.cuda.is_available() else 20
 SMALL_CROSS_ENCODER_CANDIDATES = 20 if torch.cuda.is_available() else 10
 
 nltk.download('punkt')
@@ -70,6 +72,8 @@ def search_documents(query: str, top_k: int) -> List[dict]:
     results = index.search(query_embedding, BI_ENCODER_CANDIDATES)
     results = results[0]
     results = [int(id) for id in results if id != -1]  # filter out empty results
+
+    results += Bm25Index.get().search(query, BM_25_CANDIDATES)
     # Get the paragraphs from the database
     with Session() as session:
         paragraphs = session.query(Paragraph).filter(Paragraph.id.in_(results)).all()
