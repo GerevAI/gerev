4b72b9347082f285c7be4d31eda6b3cd5c752482
Bary Levi <barylevi@gmail.com>
2023-03-07 19:06:14 +0200

    improve search

--- backend/search.py
+++ backend/search.py
@@ -28,12 +28,21 @@ class ResultPresentation:
 class Candidate:
     content: str
     score: float = 0.0
-    representation: List[ResultPresentation] = None
+    answer_start: int = -1
+    answer_end: int = -1
 
     def to_api(self) -> dict:
+        representation = []
+        # if self.answer_start > 0:
+        #     prefix = self.content[:self.answer_start]
+        #     representation.append(ResultPresentation(prefix, False))
+        representation.append(ResultPresentation(self.content[self.answer_start:self.answer_end], True))
+        if self.answer_end < len(self.content) - 1:
+            suffix = self.content[self.answer_end:]
+            representation.append(ResultPresentation(suffix, False))
         return {
             'score': self.score,
-            'content': self.representation
+            'content': representation
         }
 
 
@@ -41,25 +50,39 @@ def _cross_encode(
         cross_encoder: CrossEncoder,
         query: str,
         candidates: List[Candidate],
-        top_k: int) -> List[Candidate]:
-    scores = cross_encoder.predict([(query, candidate.content) for candidate in candidates])
+        top_k: int,
+        use_answer: bool = False) -> List[Candidate]:
+    if use_answer:
+        contents = [candidate.content[candidate.answer_start:candidate.answer_end] for candidate in candidates]
+    else:
+        contents = [candidate.content for candidate in candidates]
+    scores = cross_encoder.predict([(query, content) for content in contents])
     for candidate, score in zip(candidates, scores):
         candidate.score = score.item()
     candidates.sort(key=lambda c: c.score, reverse=True)
     return candidates[:top_k]
 
 
+def _assign_answer_sentence(candidate: Candidate, answer: str):
+    paragraph_sentences = nltk.sent_tokenize(candidate.content)
+    sentence = None
+    for i, paragraph_sentence in enumerate(paragraph_sentences):
+        if answer in paragraph_sentence:
+            sentence = paragraph_sentence
+            break
+    else:
+        sentence = answer
+    start = candidate.content.find(sentence)
+    end = start + len(sentence)
+    candidate.answer_start = start
+    candidate.answer_end = end
+
+
 def _find_answers_in_candidates(candidates: List[Candidate], query: str) -> List[Candidate]:
     for candidate in candidates:
-        qa = qa_model(question=query, context=candidate.content)
-        candidate.representation = []
-        if qa['start'] > 0:
-            prefix = candidate.content[:qa['start']]
-            candidate.representation.append(ResultPresentation(prefix, False))
-        candidate.representation.append(ResultPresentation(qa['answer'], True))
-        if qa['end'] < len(candidate.content) - 1:
-            suffix = candidate.content[qa['end']:]
-            candidate.representation.append(ResultPresentation(suffix, False))
+        answer = qa_model(question=query, context=candidate.content)
+        _assign_answer_sentence(candidate, answer['answer'])
+
     return candidates
 
 
@@ -85,4 +108,5 @@ def search_documents(query: str, top_k: int) -> List[dict]:
         # calculate large cross-encoder scores to leave just top_k candidates
         candidates = _cross_encode(cross_encoder_large, query, candidates, top_k)
         candidates = _find_answers_in_candidates(candidates, query)
+        candidates = _cross_encode(cross_encoder_large, query, candidates, top_k, use_answer=True)
         return [candidate.to_api() for candidate in candidates]
