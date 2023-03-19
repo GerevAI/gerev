43e8411570d1ff3ebc63c7c61a6bef0d6102eb00
Bary Levy <barylevi@gmail.com>
2023-03-19 15:51:12 +0000

    move telemetry to startup event

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,7 @@ cd ..
 
 sudo docker buildx create --use
 sudo docker buildx build --platform linux/amd64,linux/arm64 \
+  --cache-from type=local,src=/tmp/.buildx-cache \
+  --cache-to type=local,dest=/tmp/.buildx-cache \
   -t gerev/gerev:$VERSION . \
   -t gerev/gerev:latest --push
