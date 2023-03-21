52872e55982e353fa9fb19b867596d6ee0ec3ce5
Roey Lalazar <127092381+Roey7@users.noreply.github.com>
2023-03-21 12:53:03 +0200

    Update search-result.tsx

--- ui/src/components/search-result.tsx
+++ ui/src/components/search-result.tsx
@@ -1,30 +1,18 @@
-/* 
-This file contains the code for the <SearchResult/> Component.
-See "SearchResultProps" on this document for the the properties this component
-uses.
-*/
 
 import React from 'react';
 
-import { FaConfluence, FaSlack, FaGoogleDrive, FaUser } from "react-icons/fa";
+import BlueFolder from '../assets/images/blue-folder.svg';
+import GoogleDoc from '../assets/images/google-doc.svg';
+import GoogleDocx from '../assets/images/google-docx.svg';
+import GooglePptx from '../assets/images/google-pptx.svg';
+import { DataSourceType } from '../data-source';
 
-// This imports the full-color icons
-import Slack from '../assets/images/slack.svg';
-import Confluence from '../assets/images/confluence.svg';
-import GoogleDrive from '../assets/images/google-drive.svg'
-import BlueFolder from '../assets/images/'
-import ProfileDefault from "../assets/images/profile-picture-default.svg"
-
-const paths = {
-   "slack": Slack,
-   "confluence": Confluence,
-   "google drive": GoogleDrive
-}
 
 export interface TextPart {
    content: string
    bold: boolean
 }
+
 
 export enum ResultType {
    Docment = "document",
@@ -33,14 +21,15 @@ export enum ResultType {
    Person = "person"
 }
 
-export enum Platform {
-   Confluence = "confluence",
-   Slack = "slack",
-   GoogleDrive = "google drive"
+export enum FileType {
+   Docx = "docx",
+   Pptx = "pptx",
+   GoogleDoc = "doc",
 }
 
-// These are the attributes of a <SearchResult/> react component.
-export interface SearchResultProps {
+export interface SearchResultDetails {
+   type: ResultType
+   data_source: string
    title: string
    author: string
    author_image_url: string
@@ -49,48 +38,54 @@ export interface SearchResultProps {
    content: TextPart[]
    score: number
    location: string
-   platform: string
-   type: ResultType
+   file_type: FileType
    url: string
 }
 
+
+export interface SearchResultProps {
+   resultDetails: SearchResultDetails
+   dataSourceType: DataSourceType
+}
+
 export const SearchResult = (props: SearchResultProps) => {
+   console.log(props);
    return (
       <div className="mb-4 pt-2">
-         <a className="relative text-sm float-right text-white right-2 top-2" href={props.url}>{props.score}%</a>
+         <span className="relative text-sm float-right text-white right-2 top-2">{props.resultDetails.score.toFixed(2)}%</span>
          <div className="flex flex-row items-start">
-            <div className="relative w-[60px] h-[60px]">
-               {ResultImage(props.platform as Platform, props.type, props.author_image_url)}
-            </div>
+            {getBigIcon(props)}
             <p className='p-2 pt-0 ml-1 text-[#A3A3A3] text-sm font-poppins'>
-               <a className="text-[24px] text-[#A78BF6] text-xl font-poppins font-medium hover:underline hover:cursor-pointer" href={props.url} target='_blank'>
-                  {props.title}
+               <a className="text-[24px] text-[#A78BF6] text-xl font-poppins font-medium hover:underline hover:cursor-pointer" href={props.resultDetails.url} rel="noreferrer" target='_blank'>
+                  {props.resultDetails.title}
                </a>
                <span className="flex flex-row text-[15px] font-medium mb-4 mt-1">
                   {
-                     props.type === ResultType.Docment && <img className="inline-block mr-2" src={BlueFolder}></img>
+                     props.resultDetails.type === ResultType.Docment && <img alt="blue-folder" className="inline-block mr-2" src={BlueFolder}></img>
                   }
                   <span className="ml-0 text-[#D5D5D5]">
                      {
-                        props.type === ResultType.Message && <span>#</span>
+                        props.resultDetails.type === ResultType.Message && <span>#</span>
                      }
-                     {props.location} ·&thinsp;
+                     {props.resultDetails.location} ·&thinsp;
                   </span>
                   <span className="flex flex-row items-center">
-                     <span className='capitalize'>{props.author} ·</span>
+                     <img alt="author" className="inline-block ml-2 mr-2 h-4 rounded-xl" src={props.resultDetails.author_image_data ? props.resultDetails.author_image_data : props.resultDetails.author_image_url}></img>
+                     <span className='capitalize'>{props.resultDetails.author} ·</span>
                   </span>
                   <span>
-                     &thinsp;Updated {getFormattedTime(props.time)}&thinsp; |&thinsp;
+                     &thinsp;Updated {getFormattedTime(props.resultDetails.time)}&thinsp; |&thinsp;
                   </span>
                   <span className="flex flex-row items-center">
-                     {getSmallIconByPlatform(props.platform as Platform)}
-                     <span className="text-[#A3A3A3]">{getPlatformDisplayName(props.platform)}</span>
+                     <img alt="file-type" className="inline mx-1 fill-[#A3A3A3] h-[12px] w-[12px] grayscale"
+                        src={props.dataSourceType.image_base64}></img>
+                     <span className="text-[#A3A3A3]">{props.dataSourceType.display_name}</span>
                   </span>
                </span>
 
-               {props.type === ResultType.Docment &&
+               {props.resultDetails.type === ResultType.Docment &&
                   <span>
-                     {props.content.map((text_part, index) => {
+                     {props.resultDetails.content.map((text_part, index) => {
                         return (
                            <span key={index} style={{ wordBreak: 'break-word' }} className={(text_part.bold ? 'font-bold text-white' : '') +
                               " text-md font-poppins font-medium"}>
@@ -100,9 +95,9 @@ export const SearchResult = (props: SearchResultProps) => {
                      })}
                   </span>
                }
-               {props.type === ResultType.Message &&
+               {props.resultDetails.type === ResultType.Message &&
                   <p className="bg-[#352C45] p-2 px-4 rounded-lg font-poppins leading-[28px] border-b-[#916CCD] border-b-2">
-                     {props.content.map((text_part, index) => {
+                     {props.resultDetails.content.map((text_part, index) => {
                         return (
                            <span key={index} className={(text_part.bold ? 'font-bold text-white' : '') +
                               " fony-[14px] font-regular"}>
@@ -118,35 +113,10 @@ export const SearchResult = (props: SearchResultProps) => {
    );
 }
 
-export function getPlatformDisplayName(platform: Platform) {
-   // This converts the platform name to title case for display purposes.
-   // example input : "google drive"
-   // example output: "Google Drive"
-   let output = "";
-   for (let i = 0; i < platform.length; i++) {
-      if (i === 0 || platform.charAt(i - 1) === " ") {
-         output += platform.charAt(i).toUpperCase();
-      } else {
-         output += platform.charAt(i);
-      }
-   }
-   return output;
-}
+
 function getFormattedTime(time: string) {
    let date = new Date(time);
    return date.toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: 'numeric' });
-}
-
-function getSmallIconByPlatform(platform: Platform) {
-   let classes = "inline mx-1 fill-[#A3A3A3]";
-   switch (platform) {
-      case Platform.Confluence:
-         return <FaConfluence className={classes}></FaConfluence>
-      case Platform.Slack:
-         return <FaSlack className={classes}></FaSlack>
-      case Platform.GoogleDrive:
-         return <FaGoogleDrive className={classes}></FaGoogleDrive>
-   }
 }
 
 
@@ -174,3 +144,44 @@ function ResultImage(platform: Platform, resultType: ResultType, profilePicture 
    }
 }
 
+
+function getBigIcon(props: SearchResultProps) {
+   let classes = "mt-2 mr-2 h-[40px] w-[40px] drop-shadow-[0_0_25px_rgba(212,179,255,0.15)]";
+   let containingImage = "";
+   let onTopImage = "";
+   switch (props.resultDetails.type) {
+      case ResultType.Docment:
+         if (props.resultDetails.file_type === null) {
+            containingImage = props.dataSourceType.image_base64;
+         } else {
+            onTopImage = props.dataSourceType.image_base64;
+            switch (props.resultDetails.file_type) {
+               case FileType.Docx:
+                  containingImage = GoogleDocx;
+                  break;
+               case FileType.Pptx:
+                  containingImage = GooglePptx;
+                  break;
+               case FileType.GoogleDoc:
+                  containingImage = GoogleDoc;
+                  break;
+            }
+         }
+         break;
+      case ResultType.Message:
+         containingImage = props.resultDetails.author_image_data ? props.resultDetails.author_image_data : props.resultDetails.author_image_url;
+         onTopImage = props.dataSourceType.image_base64;
+         break;
+   }
+
+   if (onTopImage !== "") {
+      return (
+         <div className="flex flex-col items-center">
+            <img alt="file-type" className={classes} src={containingImage}></img>
+            <img alt="file-type" className="absolute mt-2 h-[24px] w-[24px]" src={onTopImage}></img>
+         </div>
+      )
+   } else {
+      return <img alt="file-type" className={classes} src={containingImage}></img>
+   }
+}
