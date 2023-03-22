023b95667e479b81487f309c73b1b1c01c809225
Bary Levy <barylevi@gmail.com>
2023-03-22 14:01:33 +0000

    Gitlab actions

--- deploy.sh
+++ deploy.sh
@@ -8,11 +8,11 @@ npm run build
 
 cd ..
 
-mkdir -p ~/.gerev/.buildx-cache
+mkdir -p $HOME/.gerev/.buildx-cache
 
 sudo docker buildx create --use
 sudo docker buildx build --platform linux/amd64,linux/arm64 \
-  --cache-from type=local,src=~/.gerev/.buildx-cache \
-  --cache-to type=local,dest=~/.gerev/.buildx-cache \
+  --cache-from type=local,src=$HOME/.gerev/.buildx-cache \
+  --cache-to type=local,dest=$HOME/.gerev/.buildx-cache \
   -t gerev/gerev:$VERSION . \
   -t gerev/gerev:latest --push
