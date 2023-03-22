85d9a5bab46c71ac3b2ea5deadfa7c61fea82501
roey <roey@gerev.ai>
2023-03-22 17:18:37 +0200

    search count

--- app/api/search.py
+++ app/api/search.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from search_logic import search_documents
+from telemetry import Posthog
 
 router = APIRouter(
     prefix='/search',
@@ -9,4 +10,5 @@ router = APIRouter(
 
 @router.get("")
 async def search(query: str, top_k: int = 5):
+    Posthog.increase_search_count()
     return search_documents(query, top_k)
