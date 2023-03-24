bd377f07a6808055f643b050564f92c70d72e210
roey <roey@gerev.ai>
2023-03-24 16:29:01 +0300

    fix npm

--- ui/src/components/data-source-panel.tsx
+++ ui/src/components/data-source-panel.tsx
@@ -14,7 +14,6 @@ import { ClipLoader } from "react-spinners";
 import { toast } from 'react-toastify';
 import { api } from "../api";
 import { ConfigField, DataSourceType } from "../data-source";
-import { colors } from "react-select/dist/declarations/src/theme";
 
 
 export interface SelectOption {
@@ -202,7 +201,7 @@ export default class DataSourcePanel extends React.Component<DataSourcePanelProp
                                        <span>1. {'Go to your Mattermost -> top-right profile picture -> Profile'}</span>
                                        <span>2. {'Security -> Personal Access Tokens -> Create token -> Name it'}</span>
                                        <span>3. {"Copy the Access Token"}</span>
-                                       <span className="text-violet-300/[.75] text-sm"> {"* Personal Access Tokens must be on"} - <a className="inline hover:underline text-violet-400/[.75]" target="_blank" href="https://developers.mattermost.com/integrate/reference/personal-access-token/">Click for more info</a></span>
+                                       <span className="text-violet-300/[.75] text-sm"> {"* Personal Access Tokens must be on"} - <a className="inline hover:underline text-violet-400/[.75]" target="_blank" rel="noreferrer" href="https://developers.mattermost.com/integrate/reference/personal-access-token/">Click for more info</a></span>
                                  </span>
                               )
                               }
