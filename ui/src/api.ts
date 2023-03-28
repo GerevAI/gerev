eb0ebb85b20ed3bb8a5cb67c7ba9045ebc44bf95
Yuval Steuer <yuval@haystack.id>
2023-03-28 20:22:53 +0300

    fix hardcoded http:// schema in baseurl

--- ui/src/api.ts
+++ ui/src/api.ts
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 let port = (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? 8000 : window.location.port;
 export const api = axios.create({
-  baseURL: `http://${window.location.hostname}:${port}`,
+  baseURL: `${window.location.protocol}//${window.location.hostname}:${port}`,
 })
