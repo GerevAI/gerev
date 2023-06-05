755265f7b149bb90e5b12c81baf976a858d529fd
Anas El Mhamdi <anas.elmhamdi@gmail.com>
2023-06-05 15:03:09 +0200

    successfully built

--- deploy.sh
+++ deploy.sh
@@ -10,9 +10,9 @@ cd ..
 
 mkdir -p $HOME/.gerev/.buildx-cache
 
-sudo docker buildx create --use
-sudo docker buildx build --platform linux/amd64,linux/arm64 \
+docker buildx create --use
+docker buildx build --platform linux/amd64,linux/arm64 \
   --cache-from type=local,src=$HOME/.gerev/.buildx-cache \
   --cache-to type=local,dest=$HOME/.gerev/.buildx-cache \
-  -t gerev/gerev:$VERSION . \
-  -t gerev/gerev:latest --push
+  -t us-central1-docker.pkg.dev/gorgias-growth-production/growth-ops/gerev:$VERSION . \
+  -t us-central1-docker.pkg.dev/gorgias-growth-production/growth-ops/gerev:latest --push
